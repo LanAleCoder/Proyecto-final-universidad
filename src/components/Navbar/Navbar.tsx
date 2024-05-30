da6776003297fbb35414cfd10c3b7a6439ca35d4
pjaime1623 <151006129+pjaime1623@users.noreply.github.com>
2024-05-29 23:41:44 -0600

    Navbar.tsx
    nav

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,12 +4,30 @@ const Navbar: React.FC = () => {
 
     return (
       <nav className="navbar">
-        <div className="navbar-brand">Mi Restaurante</div>
+        <div className="navbar-brand">Sabores del Alma</div>
         <ul className="navbar-menu">
           <li className="navbar-item"><a href="#home">Inicio</a></li>
-          <li className="navbar-item"><a href="#menu">Menú</a></li>
-          <li className="navbar-item"><a href="#about">Nosotros</a></li>
-          <li className="navbar-item"><a href="#contact">Contacto</a></li>
+          <li className="navbar-item"><a href="#menu">Menú</a>
+          <ul>
+            <li><a href="#">Menú</a></li>
+            <li><a href="#">Pedidos</a></li>
+            <li><a href="#">Inventario</a></li>
+          </ul>
+          </li>
+          <li className="navbar-item"><a href="#about">Nosotros</a>
+          <ul>
+            <li><a href="#">Eventos</a></li>
+            <li><a href="#">Aforo</a></li>
+            <li><a href="#">Historia</a></li>
+            <li><a href="#">Ventas</a></li>
+          </ul>
+          </li>
+          <li className="navbar-item"><a href="#contact">Contacto</a>
+          <ul>
+            <li><a href="#">Contacto</a></li>
+            <li><a href="#">Reservaciones</a></li>
+          </ul>
+          </li>
         </ul>
       </nav>
     );
